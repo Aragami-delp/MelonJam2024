1dfeb89e64d6862947925e51dc721925a3e2d513
Fuchsiano <Fuchsiano04@gmail.com>
2024-12-29 18:32:47 +0100

    upgradeFix

--- MelonJam2024Unity/Assets/Scripts/SceneChanger.cs
+++ MelonJam2024Unity/Assets/Scripts/SceneChanger.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
-public class SceneChanger : MonoBehaviour
+[DefaultExecutionOrder(-9000)]
+public class UpgradeUiManager : MonoBehaviour
 {
+    public static UpgradeUiManager Instance; 
+
+    public Transform UpgradeParent;
+    public Transform Details; 
+    
+
+    private void Awake()
+    {
+        Instance = this; 
+    }
+
     public void StartGame() 
     {
         SceneManager.LoadScene("GameScene");
